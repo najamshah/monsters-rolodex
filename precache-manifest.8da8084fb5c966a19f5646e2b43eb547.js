d19efa31611e2e3906770f6bbef690403104cca9
najamshah <53971636+najamshah@users.noreply.github.com>
2019-08-10 17:46:07 +0400

    first
    dd

---
+++ precache-manifest.8da8084fb5c966a19f5646e2b43eb547.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ebb3cd462dbc4a64090381b3d3bbbc7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "30b6da6cb0273a919705",
+    "url": "/monsters-rolodex/static/css/main.0e8a2494.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "30b6da6cb0273a919705",
+    "url": "/monsters-rolodex/static/js/main.0cf38978.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
